9c0d802ce9c106cab783e3a06940d164750588d0
Zairaxx <brandontsegai@gmail.com>
2025-08-27 12:04:54 +0200

    added comments

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -20,6 +20,7 @@ const User = ({user}) => {
         <p>Namn: {user.name}</p>
         <button onClick={() => setShowInfo(!showInfo)}>Toggle info</button>
         {showInfo && <div>{user.age < 30 ? <p>Ålder: {user.age}</p> : <p>Ålder:30+</p>}
+        {/* Vi ser till att vi kollar om location finns i användarens data innan vi skriver ut city */}
         <p>Stad: {user.location?.city}</p></div>}
     </div>)
 }
